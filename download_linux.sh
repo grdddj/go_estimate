7cfc0ac1cc869399ac61621b42187c21b2840db7
grdddj <jiri.musil06@seznam.cz>
2024-10-05 15:58:08 +0200

    chore: add katago download script for linux

---
+++ download_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download Linux no-GPU version binary + model
+
+wget https://grdddj.eu/go/katago_linux_eigen_cpu -O katago
+chmod +x katago
+wget https://grdddj.eu/go/katago.bin.gz -O katago.gz
